138601b78c4f7dc8e4d0bee5f1f3812ce6d57097
AdrianSzklarski <adrian_it@o2.pl>
2023-02-22 23:19:58 +0100

    Added Note file

---
+++ note.py
@@ -0,0 +1,13 @@
+# TypeError: Descriptors cannot not be created directly
+# pip install protobuf==3.20.*
+
+# ---------------------------------------------------------
+# 2023-02-22 23:18:31.604 WARNING root:
+#   Warning: to view this Streamlit app on a browser, run it with the following
+#   command:
+#
+#     streamlit run /home/adrian/Pulpit/GitHub_Public/Dectenion_face/camera_strem.py [ARGUMENTS]
+#
+# Process finished with exit code 0
+
+# I have to run app form termial: streamlit run camera_strem.py
