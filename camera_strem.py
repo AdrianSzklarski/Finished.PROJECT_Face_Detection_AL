193e1f3274b44c21c383de67f6d9a857ed2b8655
AdrianSzklarski <adrian_it@o2.pl>
2023-02-23 23:34:30 +0100

    Added set window

--- camera_strem.py
+++ camera_strem.py
@@ -1,7 +1,19 @@
+import tkinter as tk
+
 
 class CameraStrem:
     def __init__(self, root):
         self.root = root
 
+        self.get_set_window()
+
+    def get_set_window(self):
+        self.root.title("Face Detection create by A.Szklarski 02.2023")
+        self.root.geometry('800x640')
 
 
+# Test run, after delete
+if __name__ == '__main__':
+    root = tk.Tk()
+    CameraStrem(root)
+    root.mainloop()
